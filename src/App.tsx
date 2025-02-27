b53ba0d8c8108dae16e96973dbd53ff0dc60dbb7
GvantsaGongadzegit config --global user.email ggong2016@agruni.edu.ge <ggong2016@agruni.edu.ge>
2025-02-27 19:04:34 +0400

    added prerrier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
 
 function App() {
-
-  return (
-   <div className="text-3xl font-bold underline">App Component</div>
-  )
+  return <div className="text-3xl font-bold underline">App Component</div>
 }
 
 export default App
